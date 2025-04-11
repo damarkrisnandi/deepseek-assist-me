7ef76abb624a2d7580175617b63617cff8c51dd1
damar <damar@gitlab.lab>
2025-04-11 13:57:09 +0700

    flex-start flex-end

--- src/components/main/AppChat.tsx
+++ src/components/main/AppChat.tsx
@@ -1,5 +1,6 @@
 import { Card, Avatar } from "@chakra-ui/react";
 import Markdown from "react-markdown";
+import { Prose } from "../ui/prose"
 
 // const { Text } = Typography;
 
@@ -17,23 +18,28 @@ const AppChat: React.FC<MessageCardProps> = ({
   model
 }: MessageCardProps) => {
   return (
-    <Card.Root variant={"subtle"} minW="80vh">
-      <Card.Body gap="2">
-        {avatar && (
-          <Avatar.Root size="sm" shape="rounded">
-            <Avatar.Image src={avatar || "https://picsum.photos/200/300"} />
-            <Avatar.Fallback name="Nue Camp" />
-          </Avatar.Root>
-        )}
-        <Card.Title mb="0">{role}</Card.Title>
-        { model && <Card.Description>
-          { model }
-        </Card.Description> }
-        {/* <Card.Description> */}
-        <Markdown>{message}</Markdown>
-        {/* </Card.Description> */}
-      </Card.Body>
-    </Card.Root>
+    <div style={{ display: 'flex', width: '100%', justifyContent: (role === 'assistant') ? "flex-start" : "flex-end" }}>
+      <Card.Root variant={"subtle"} minW={(role === 'assistant') ? "80%" : "50%"} backgroundColor={(role === 'assistant') ? "" : "aquamarine"}>
+        <Card.Body gap="2">
+          {avatar && (
+            <Avatar.Root size="sm" shape="rounded">
+              <Avatar.Image src={avatar || "https://picsum.photos/200/300"} />
+              <Avatar.Fallback name="Nue Camp" />
+            </Avatar.Root>
+          )}
+          <Card.Title mb="0">{role}</Card.Title>
+          { model && <Card.Description>
+            { model }
+          </Card.Description> }
+
+          <Prose minWidth={'100%'}>
+            <Markdown>{message}</Markdown>
+          </Prose>
+
+        </Card.Body>
+      </Card.Root>
+    </div>
+    
   );
 };
 
