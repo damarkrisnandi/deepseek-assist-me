21114af3820b7eb39b14dfdfdfee5c684501416c
damar <damar@gitlab.lab>
2025-04-13 22:05:09 +0700

    update UI

--- src/components/main/AppChat.tsx
+++ src/components/main/AppChat.tsx
@@ -1,8 +1,19 @@
-import { Box, Badge, Stack, Skeleton, Spinner, Flex, Text, Clipboard, IconButton } from "@chakra-ui/react";
+import {
+  Box,
+  Badge,
+  Stack,
+  Skeleton,
+  Spinner,
+  Flex,
+  Text,
+  Clipboard,
+  IconButton,
+} from "@chakra-ui/react";
 import Markdown from "react-markdown";
 import { Prose } from "../ui/prose";
-import rehypeHighlight from 'rehype-highlight';
-import remarkGfm from 'remark-gfm'
+import rehypeHighlight from "rehype-highlight";
+import remarkGfm from "remark-gfm";
+import React from "react";
 
 // const { Text } = Typography;
 
@@ -19,56 +30,170 @@ const AppChat: React.FC<MessageCardProps> = ({
   message,
   role,
   model,
-  isRunning
+  isRunning,
 }: MessageCardProps) => {
-  const isAssistant = () => (role === 'assistant')
+  const isAssistant = () => role === "assistant";
+  if (isAssistant()) {
+    return (
+      <>
+        <AssistantChat
+          message={message}
+          role={role}
+          model={model}
+          isRunning={isRunning}
+        />
+      </>
+    );
+  }
   return (
-    <div style={{ display: 'flex', width: '100%', justifyContent: (isAssistant()) ? "flex-start" : "flex-end" }}>
-      <Box display={'flex'} flexDirection={'column'} justifyContent={"flex-start"} textAlign={"left"} minW={(isAssistant()) ? "90%" : "70%"} gap={'0.5em'}>
-        <Flex gap={'0.5em'}>
-          <Badge colorPalette={isAssistant() ? "teal" : "yellow"} variant="solid">{ role }</Badge>
-          { model && <Badge colorPalette={"black"} variant={"solid"}>{ model }</Badge> }
-        </Flex>
-        
-        <Box bg={(isAssistant()) ? "#f3f3f3" : "#d5e5F5"} p={"4"} textAlign={"left"} minW={(isAssistant()) ? "90%" : "70%"} justifyContent={"flex-start"} gap={"0.3em"}>
-          <Prose minWidth={'100%'}>
-            <Markdown rehypePlugins={[rehypeHighlight, remarkGfm]}>{message}</Markdown>
-          </Prose>
-          { isRunning &&
-          <Flex gap={'1em'} alignItems={'center'}>
-            <Text fontSize={"2xs"}>RUNNING</Text>
-            <Spinner />
-          </Flex> 
-            
-          }
-
-          {
-            isAssistant() && !isRunning &&
-            <Flex justifyContent={'flex-end'}>
-              <Copy value={message} />
-            </Flex>
-          }
-        </Box>
-      </Box>
-      
-    </div>
-    
+    <>
+      <MyChat message={message} role={role} model={model} />
+    </>
   );
 };
 
-export const SkeletonChat : React.FC<Partial<MessageCardProps>> = ({
+const MyChat: React.FC<MessageCardProps> = ({ message, role, model }) => {
+  return (
+    <div style={{ display: "flex", width: "100%", justifyContent: "flex-end" }}>
+      <Box
+        display={"flex"}
+        flexDirection={"column"}
+        justifyContent={"flex-start"}
+        textAlign={"left"}
+        minW={"70%"}
+        gap={"0.5em"}
+      >
+        <Flex gap={"0.5em"}>
+          <Badge colorPalette={"yellow"} variant="solid">
+            {role}
+          </Badge>
+          {model && (
+            <Badge colorPalette={"black"} variant={"solid"}>
+              {model}
+            </Badge>
+          )}
+        </Flex>
+
+        <Box
+          bg={"#d5e5F5"}
+          p={"4"}
+          textAlign={"left"}
+          minW={"70%"}
+          justifyContent={"flex-start"}
+          gap={"0.3em"}
+          borderBottomLeftRadius={"xl"}
+          borderBottomRightRadius={"2xl"}
+          borderTopLeftRadius={"xl"}
+        >
+          <Prose minWidth={"100%"}>
+            <Markdown rehypePlugins={[rehypeHighlight, remarkGfm]}>
+              {message}
+            </Markdown>
+          </Prose>
+        </Box>
+      </Box>
+    </div>
+  );
+};
+
+const AssistantChat: React.FC<MessageCardProps> = ({
+  message,
   role,
-  model
+  model,
+  isRunning,
+}) => {
+  return (
+    <div
+      style={{ display: "flex", width: "100%", justifyContent: "flex-start" }}
+    >
+      <Box
+        display={"flex"}
+        flexDirection={"column"}
+        justifyContent={"flex-start"}
+        textAlign={"left"}
+        minW={"90%"}
+        gap={"0.5em"}
+      >
+        <Flex gap={"0.5em"}>
+          <Badge colorPalette={"teal"} variant="solid">
+            {role}
+          </Badge>
+          {model && (
+            <Badge colorPalette={"black"} variant={"solid"}>
+              {model}
+            </Badge>
+          )}
+        </Flex>
+
+        <Box
+          bg={"#f3f3f3"}
+          p={"4"}
+          textAlign={"left"}
+          minW={"90%"}
+          justifyContent={"flex-start"}
+          gap={"0.3em"}
+          borderBottomLeftRadius={"2xl"}
+          borderBottomRightRadius={"xl"}
+          borderTopRightRadius={"xl"}
+        >
+          <Prose minWidth={"100%"}>
+            <Markdown rehypePlugins={[rehypeHighlight, remarkGfm]}>
+              {message}
+            </Markdown>
+          </Prose>
+          {isRunning && (
+            <Flex gap={"1em"} alignItems={"center"}>
+              <Text fontSize={"2xs"}>RUNNING</Text>
+              <Spinner />
+            </Flex>
+          )}
+
+          {!isRunning && (
+            <Flex justifyContent={"flex-end"}>
+              <Copy value={message} />
+            </Flex>
+          )}
+        </Box>
+      </Box>
+    </div>
+  );
+};
+
+export const SkeletonChat: React.FC<Partial<MessageCardProps>> = ({
+  role,
+  model,
 }: Partial<MessageCardProps>) => {
   return (
-    <div style={{ display: 'flex', width: '100%', justifyContent: "flex-start" }}>
-      <Box display={'flex'} flexDirection={'column'} justifyContent={"flex-start"} textAlign={"left"} minW={"90%"} gap={'0.5em'}>
-        <Flex gap={'0.5em'}>
-          <Badge colorPalette={"teal"} variant="solid">{ role }</Badge>
-          <Badge colorPalette={"black"} variant={"solid"}>{ model }</Badge>
+    <div
+      style={{ display: "flex", width: "100%", justifyContent: "flex-start" }}
+    >
+      <Box
+        display={"flex"}
+        flexDirection={"column"}
+        justifyContent={"flex-start"}
+        textAlign={"left"}
+        minW={"90%"}
+        gap={"0.5em"}
+      >
+        <Flex gap={"0.5em"}>
+          <Badge colorPalette={"teal"} variant="solid">
+            {role}
+          </Badge>
+          <Badge colorPalette={"black"} variant={"solid"}>
+            {model}
+          </Badge>
         </Flex>
-        <Box bg={"#f3f3f3"} p={"4"} textAlign={"left"} minW={"90%"} justifyContent={"flex-start"} gap={"0.3em"}>
-            
+        <Box
+          bg={"#f3f3f3"}
+          p={"4"}
+          textAlign={"left"}
+          minW={"90%"}
+          justifyContent={"flex-start"}
+          gap={"0.3em"}
+          borderBottomLeftRadius={"2xl"}
+          borderBottomRightRadius={"xl"}
+          borderTopRightRadius={"xl"}
+        >
           <Stack flex="1" mt={"1em"}>
             <Skeleton height="5" />
             <Skeleton height="5" width="80%" />
@@ -77,18 +202,18 @@ export const SkeletonChat : React.FC<Partial<MessageCardProps>> = ({
       </Box>
     </div>
   );
-}
+};
 
-const Copy = ({ value }: { value: string } ) => {
+const Copy = ({ value }: { value: string }) => {
   return (
-    <Clipboard.Root value={ value }>
+    <Clipboard.Root value={value}>
       <Clipboard.Trigger asChild>
         <IconButton variant="surface" size="xs">
           <Clipboard.Indicator />
         </IconButton>
       </Clipboard.Trigger>
     </Clipboard.Root>
-  )
-}
+  );
+};
 
 export default AppChat;
--- src/components/main/AppChatContainer.tsx
+++ src/components/main/AppChatContainer.tsx
@@ -1,26 +1,28 @@
+import React, { useEffect, useState } from "react";
+import AppChatInputMsg from "./AppChatInputMsg";
+import AppChat, { SkeletonChat } from "./AppChat";
+import { EmptyState, Flex, Stack, VStack } from "@chakra-ui/react";
+import { chatAssistant } from "../../services/index";
 
-import React, { useEffect, useState } from 'react';
-import AppChatInputMsg from './AppChatInputMsg';
-import AppChat, { SkeletonChat } from './AppChat';
-import { EmptyState, Flex, Stack, VStack } from '@chakra-ui/react';
-import { chatAssistant } from "../../services/index"
-
-import ndjsonStream from 'can-ndjson-stream';
-import { LuMessageCircle } from 'react-icons/lu';
+import ndjsonStream from "can-ndjson-stream";
+import { LuMessageCircle } from "react-icons/lu";
 
 // interface AppChatContainerProps {
 //   messages: { role: string; content: string }[];
 // }
 
 const AppChatContainer: React.FC = () => {
-
   const [messages, setMessages] = useState<any[]>([]);
   const [tempMessage, setTempMessage] = useState<any>([]);
   const [tempModel, setTempModel] = useState<any>(null);
-  const [aiStatus, setAiStatus] = useState<{ loading: boolean, running: boolean, done: boolean }>({ loading: false, running: false, done: false })
+  const [aiStatus, setAiStatus] = useState<{
+    loading: boolean;
+    running: boolean;
+    done: boolean;
+  }>({ loading: false, running: false, done: false });
   // const { messages, addDefaultMessages, addMessages} = useMessageStore((state: any) => state)
   const scrollToBottom = () => {
-    const el = document.getElementById('messages');
+    const el = document.getElementById("messages");
     if (el) {
       el.scrollTo(0, el.scrollHeight);
     }
@@ -28,90 +30,108 @@ const AppChatContainer: React.FC = () => {
 
   const handleChat = (data: any) => {
     setTempMessage([]);
-    setMessages([...messages,  { role: 'user', content: data.message }])
-    setAiStatus({...aiStatus, loading: true});
+    setMessages([...messages, { role: "user", content: data.message }]);
+    setAiStatus({ ...aiStatus, loading: true });
     setTimeout(() => {
+      chatAssistant(
+        [...messages, { role: "user", content: data.message }],
+        tempModel
+      ).then(async (response) => {
+        await handleNextMessage(data, response);
+      });
+    }, 400);
+  };
 
-      chatAssistant([...messages, { role: 'user', content: data.message }], tempModel)
-      .then(async (response) => {
-        setAiStatus({...aiStatus, loading: false, running: true});
-        const reader = ndjsonStream(response.body).getReader();
-        let newline: any;
-        let line = []
-        let tempMsg: any = null;
-        do {
-          newline = await reader.read();
-          if (newline.value) {
-            line.push(newline.value.message.content)
-            setTempMessage([...tempMessage, ...line]);
-            setTempModel(newline.value.model)
-            scrollToBottom();
-            tempMsg = {content: line.join(''), role: newline.value?.message.role, model: newline.value.model };
-            setMessages([...messages, { content: data.message, role: "user" }])
-          } 
-          
-          
-          if (newline.done) {
-            setAiStatus({ loading: false, running: false, done: true });
-            setMessages([...messages, { content: data.message, role: "user" }, tempMsg])
-            setTempMessage([])
-          }
-        } while (!newline.done) 
+  // Core Helper Functions
+  const handleNextMessage = async (data: any, response: any) => {
+    setAiStatus({ ...aiStatus, loading: false, running: true });
+    const reader = ndjsonStream(response.body).getReader();
+    let newline: any;
+    let line = [];
+    let tempMsg: any = null;
+    do {
+      newline = await reader.read();
+      if (newline.value) {
+        line.push(newline.value.message.content);
+        setTempMessage([...tempMessage, ...line]);
+        setTempModel(newline.value.model);
+        scrollToBottom();
+        tempMsg = {
+          content: line.join(""),
+          role: newline.value?.message.role,
+          model: newline.value.model,
+        };
+        setMessages([...messages, { content: data.message, role: "user" }]);
+      }
 
-
-      })
-
-    }, 400)
-    
-
-  }
+      if (newline.done) {
+        setAiStatus({ loading: false, running: false, done: true });
+        setMessages([
+          ...messages,
+          { content: data.message, role: "user" },
+          tempMsg,
+        ]);
+        setTempMessage([]);
+      }
+    } while (!newline.done);
+  };
 
   useEffect(() => {
     scrollToBottom();
   }, [messages]);
 
   return (
-    <Flex gap="2" height={"90vh"} direction={'column'} justifyContent={'flex-end'}>
-        <Flex gap="2" direction={'column'} overflowY={'auto'} id="messages">
-        {
-          messages.length === 0 && <EmptyMessages />
-        }
-        {
-          messages.map((message: any, index: number) => (
-            <AppChat message={message.content} role={message.role} model={message.model} key={index}/>
-          ))
-        }
-        { aiStatus.loading && <SkeletonChat model={tempModel} role={'assistant'} /> }
-        { tempMessage.length > 0 && <AppChat message={tempMessage.join('')} model={tempModel} role={'assistant'} isRunning={aiStatus.running} /> }
-        
-        </Flex>
-        <AppChatInputMsg onChat={handleChat} onChangeModel={setTempModel} />
+    <Flex
+      gap="2"
+      height={"90vh"}
+      direction={"column"}
+      justifyContent={"flex-end"}
+    >
+      <Flex gap="2" direction={"column"} overflowY={"auto"} id="messages">
+        {messages.length === 0 && <EmptyMessages />}
+        {messages.map((message: any, index: number) => (
+          <AppChat
+            message={message.content}
+            role={message.role}
+            model={message.model}
+            key={index}
+          />
+        ))}
+        {aiStatus.loading && (
+          <SkeletonChat model={tempModel} role={"assistant"} />
+        )}
+        {tempMessage.length > 0 && (
+          <AppChat
+            message={tempMessage.join("")}
+            model={tempModel}
+            role={"assistant"}
+            isRunning={aiStatus.running}
+          />
+        )}
+      </Flex>
+      <AppChatInputMsg onChat={handleChat} onChangeModel={setTempModel} />
     </Flex>
-      
-      
-      
-   
   );
 };
 
 const EmptyMessages = () => {
   return (
-    <Stack h={'50vh'}>
-    <EmptyState.Root size={'md'}>
-      <EmptyState.Content>
-        <EmptyState.Indicator>
-          <LuMessageCircle />
-        </EmptyState.Indicator>
-        <VStack textAlign="center">
-          <EmptyState.Title>Deepseek Assist me!</EmptyState.Title>
-          <EmptyState.Description>
-            Ask Something about programming terms and topics
-          </EmptyState.Description>
-        </VStack>
-      </EmptyState.Content>
-    </EmptyState.Root>
+    <Stack h={"50vh"}>
+      <EmptyState.Root size={"md"}>
+        <EmptyState.Content>
+          <EmptyState.Indicator>
+            <LuMessageCircle />
+          </EmptyState.Indicator>
+          <VStack textAlign="center">
+            <EmptyState.Title>Deepseek Assist me!</EmptyState.Title>
+            <EmptyState.Description>
+              Ask Something about programming terms and topics
+            </EmptyState.Description>
+          </VStack>
+        </EmptyState.Content>
+      </EmptyState.Root>
     </Stack>
-  )
-}
+  );
+};
 
 export default AppChatContainer;
